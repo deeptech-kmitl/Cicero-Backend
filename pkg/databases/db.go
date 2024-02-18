234a98c7d0dcd330fcf2bf3ccb24913d2f304054
FixFire <89625074+NatthawutSK@users.noreply.github.com>
2024-02-18 11:25:44 +0700

    fixed bug : Timezone :bug:

--- pkg/databases/db.go
+++ pkg/databases/db.go
@@ -14,6 +14,12 @@ func DbConnect(cfg config.IDbConfig) *sqlx.DB {
 	if err != nil {
 		log.Fatalf("connect to db failed: %v\n", err)
 	}
+	// Set time zone
+	_, err = db.Exec("SET TIME ZONE 'Asia/Bangkok'")
+	if err != nil {
+		log.Fatalf("set time zone failed: %v\n", err)
+	}
+
 	db.DB.SetMaxOpenConns(cfg.MaxOpenConns())
 	return db
 }
